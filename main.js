e48db6035355a35d3b3b8ff6b1f0162cf0959dd5
PaytonAnderson <payton@clickityclick.com>
2025-12-01 12:00:38 -0800

    Configure WebSocketserver address

--- main.js
+++ main.js
@@ -71,13 +71,22 @@ function sendMoves(board, websocket) {
     });
 }
 
+function getWebSocketServer() {
+    if (window.location.host === "PaytonAnderson.github.io") {
+        return "wss://elegant-nicolina-payton-personal-1943c6ad.koyeb.app/";
+    } else if (window.location.host === "localhost:8000") {
+        return "ws://localhost:8001/";
+    } else {
+        throw new Error(`Unsupported host: ${window.location.host}`);
+    }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     // Initialize the UI.
     const board = document.querySelector(".board");
     createBoard(board);
     // Open the WebSocket connection and register event handlers.
-    const websocket = new WebSocket("ws://localhost:8001/");
-    initGame(websocket);
+    const websocket = new WebSocket(getWebSocketServer()); initGame(websocket);
     receiveMoves(board, websocket);
     sendMoves(board, websocket);
 });
